97e50f82ff3abffa26617bbd492e8a4a7e2f310e
TANSIM AHMED TASHDID <tansimahmedtasjid@gmail.com>
2024-12-23 23:00:42 +0600

    project setup

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {images: {
+  remotePatterns: [
+    {
+      protocol: 'https',
+      hostname: '**', // Allows any hostname
+      pathname: '/**', // Allows any path
+    },
+  ],
+},};
 
 export default nextConfig;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,12 +6,17 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      boxShadow: {
+        shadowOne: "10px 10px 19px #1c1e22, -10px -10px 19px #262a2e",
+      },
+      colors: {
+        bodyColor: "#212428",
+        lightText: "#c4cfde",
+        boxBg: "linear-gradient(145deg, #1e2024, #23272b)",
+        designColor: "#fcc203",
       },
     },
   },
